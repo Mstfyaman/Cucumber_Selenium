75f5f28ee51aa86fe837eff16991066eb2d33ed9
Mstfyaman <forfreedom067@gmail.com>
2022-12-01 21:33:46 +0300

    01.12.2022 İntroduction to Cucumber

---
+++ src/test/java/com/krafttech/stept_definitions/Login_stepDefs.java
@@ -0,0 +1,35 @@
+package com.krafttech.stept_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_stepDefs {
+
+    @Given("The user is on the Login page")
+    public void the_user_is_on_the_login_page() {
+
+        System.out.println("Batch 2 was here");
+        System.out.println("O open the browser and navigate to Krafttech web page");
+
+    }
+    @When("The user enters Mike credentials")
+    public void the_user_enters_mike_credentials() {
+        System.out.println("I enter Mike username and password and click login button");
+
+    }
+    @Then("The user should be able to login")
+    public void the_user_shoult_be_able_to_login() {
+        System.out.println("I verify that url changed Dashboard page");
+    }
+
+    @When("The user enters Sebastian credentials")
+    public void theUserEntersSebastianCredentials() {
+        System.out.println("I enter Sebastian username and password and click login button");
+    }
+
+    @When("The user enters Jhon Nash credentials")
+    public void theUserEntersJhonNashCredentials() {
+        System.out.println("I enter Jhon Nash username and password and click login button");
+    }
+}
